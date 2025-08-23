322abfb338a45d35aff2234fee3d1f8e15c2cf47
reignu <ungier.adam@gmail.com>
2025-08-23 12:53:07 +0100

    add backend project to backend folder

---
+++ backend/src/main/java/com/PL/premier_hub/PremierHubApplication.java
@@ -0,0 +1,13 @@
+package com.PL.premier_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierHubApplication.class, args);
+	}
+
+}
