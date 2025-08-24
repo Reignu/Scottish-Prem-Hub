3dfdc41c39d41dbabe2ded79fe98ff95b17ac1ab
reignu <ungier.adam@gmail.com>
2025-08-24 22:27:25 +0100

    Update Player service functionality

--- backend/src/main/java/com/PL/premier_hub/player/PlayerService.java
+++ backend/src/main/java/com/PL/premier_hub/player/PlayerService.java
@@ -1,9 +1,11 @@
 package com.PL.premier_hub.player;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -44,5 +46,30 @@ public class PlayerService {
         return playerRepository.findAll().stream().filter(player ->
                 teamName.equals(player.getTeam()) && position.equals(player.getPosition()))
                 .collect(Collectors.toList());
+    }
+
+    public Player addPlayer(Player player){
+        return playerRepository.save(player);
+    }
+
+    public Player updatePlayer(Player updatedPlayer){
+        Optional<Player> existingPlayer = playerRepository.findByName(updatedPlayer.getName());
+
+        if(existingPlayer.isPresent()){
+            Player playerToUpdate = existingPlayer.get();
+            playerToUpdate.setName(updatedPlayer.getName());
+            playerToUpdate.setNation(updatedPlayer.getNation());
+            playerToUpdate.setPosition(updatedPlayer.getPosition());
+            playerToUpdate.setTeam(updatedPlayer.getTeam());
+
+            playerRepository.save(playerToUpdate);
+            return playerToUpdate;
+        }
+        return null;    // No player found in the database
+    }
+
+    @Transactional
+    public void deletePlayer(String playerName){
+        playerRepository.deleteByName(playerName);
     }
 }
